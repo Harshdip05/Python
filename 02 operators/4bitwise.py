87331b3bd0ad681acc0121bf3e90b0d4685948c4
Harshdip05 <harshdipnandre05@gmail.com>
2026-01-20 02:54:51 +0000

    bitwise

---
+++ 02 operators/4bitwise.py
@@ -0,0 +1,10 @@
+# Bitwise operators work on binary numbers (0 and 1).
+# Instead of operating on whole numbers, they operate on each bit.
+
+print(2 & 3)
+print(2 | 3)
+print(2 ^ 3)
+print(~2)      # unary works on only one number
+print(2 << 3)
+print(2 >> 3)
+      
