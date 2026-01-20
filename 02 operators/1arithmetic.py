7653bbb709dbb2b80b9b9b0d7fb75eab6ef3f758
Harshdip05 <harshdipnandre05@gmail.com>
2026-01-20 02:25:02 +0000

    arithmetic operator

---
+++ 02 operators/1arithmetic.py
@@ -0,0 +1,34 @@
+# Arithmetic operators are used to perform mathematical calculations.
+
+print(11 + 4)
+print(11 - 4)
+print(11 / 4)
+print(11 * 4)
+print(11 % 4)    #modulus
+print(11 // 4)   #quotient=floor division
+print(11 ** 4)   #exponent(power of)
+
+print()
+
+print(-10/3)
+print(-10//3)
+print(-10%3)
+
+print()
+
+print(10/-3)
+print(10//-3)
+print(10%-3)
+# a = (b × (a // b)) + (a % b)  (for remainder)
+# Python uses floor division, not truncate division.
+
+
+print()
+
+# Operator Precedence (Order)
+# ()
+# **
+# *, /, //, %
+# +, -
+
+# associativity
