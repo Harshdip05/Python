767b55483bfce49f9fce59e41097a5f210410460
Harshdip05 <harshdipnandre05@gmail.com>
2026-01-20 02:01:32 +0000

    print function

---
+++ 01 basics/9print.py
@@ -0,0 +1,4 @@
+# print() → Calling print with no arguments inserts a blank line.
+# print(' ') → Prints a single space, effectively creating a blank line in many cases.
+# print(" ") → Same as above, but with double quotes.
+# print("\n") → Uses the newline character to produce an empty line.
