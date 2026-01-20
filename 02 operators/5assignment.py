d08bb36dad4ec0ebde8a3a160f44b6a4ba1dbd0a
Harshdip05 <harshdipnandre05@gmail.com>
2026-01-20 03:01:38 +0000

    assign values to variables

---
+++ 02 operators/5assignment.py
@@ -0,0 +1,31 @@
+# Assignment operators are used to assign values to variables.
+
+a = 10    # (basic assignment) store value 10 in variable a
+print(a)
+
+a += 1    # add and assign  a = a+1
+print(a)
+
+a -= 1
+print(a)
+
+a *= 1
+print(a)
+
+a /= 1 
+print(a)
+
+a //=1    # floor divide and assign   a = a//1
+print(a)
+
+# a &= 2
+# print(a)
+
+print() 
+
+a = 10
+a &= 1
+print(a)
+
+
+# not a++ and ++a
