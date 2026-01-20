abbdb3465fec034dd293fc2c196c575a3070e356
Harshdip05 <harshdipnandre05@gmail.com>
2026-01-20 02:30:01 +0000

    relational=comparsion

---
+++ 02 operators/2relational.py
@@ -0,0 +1,21 @@
+# Relational operators and comparison operators are the same thing.
+#    Relational → shows relationship between values
+#    Comparison → compares two values
+#                                               Both mean: “Compare two values and return True or False.”
+
+print(11==4)
+print(11>4)
+print(11<4)
+print(11>=4)
+print(11<=4)
+print(11!=4)
+
+print()
+
+
+# if a = 4:   # wrong
+a = 10
+if a == 4:
+    print("true")
+else:
+    print("false")    
