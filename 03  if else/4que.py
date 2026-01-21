21482f377739bf256feb2bc1f44839dcd993f0fd
Harshdip05 <harshdipnandre05@gmail.com>
2026-01-21 02:45:13 +0000

    Practice question

--- 03  if else/4que.py
+++ 03  if else/4que.py
@@ -15,3 +15,27 @@ elif op=="/":
     print("division: ",num1/num2)     
 else:
     print("you entered wrong op")    
+
+
+
+print()
+
+
+menu = input('''
+HI! How can I help  you
+1. Enter 1 for pin change
+2. Enter 2 for balance check
+3. Enter 3 for withdrawal
+4. Enter for exit 
+''')
+
+if menu == "1":
+    print("pin change")
+elif menu == "2":
+    print("balance")
+elif menu=="3":
+    print("withdrawal")    
+elif menu=="4":
+    print("exit")
+else:
+    print("entered wrong menu")    
