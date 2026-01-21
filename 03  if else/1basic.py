0efd17820a5b003508fc65f2a7e621dbb30641a0
Harshdip05 <harshdipnandre05@gmail.com>
2026-01-21 02:26:23 +0000

    Learn if elif else

---
+++ 03  if else/1basic.py
@@ -0,0 +1,36 @@
+# if condition:
+    # code runs if condition is True
+
+# elif another_condition:
+    # runs if above condition is False
+    # and this condition is True
+
+# else:
+    # runs if all conditions are False
+
+
+# if–elif–else is used for decision making.
+        # It allows your program to:
+        # run different code based on different conditions.
+# indentation concept 
+
+
+# more than two conditions then use elif
+
+age = 18
+if age >= 18:
+    print("You can vote")
+else:
+    print("you cannot vote")
+
+
+# Put the MOST SPECIFIC or MOST RESTRICTIVE condition at the TOP.
+# Put the general conditions at the BOTTOM. 
+marks = 80 
+if marks >= 90:
+    print("Grade A")
+elif marks >= 60:
+    print("Pass")
+
+# else means: “Everything not handled above.”    
+  
