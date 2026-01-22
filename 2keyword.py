917df5644f86b7d6da4e007ee2255d0848f6e6dd
Harshdip Nandre <harshdipnandre05@gmail.com>
2026-01-22 16:03:05 +0000

    Keyword module

---
+++ 2keyword.py
@@ -0,0 +1,14 @@
+# Keywords are words that:
+    # have special meaning in Python
+    # cannot be used as variable names
+    # are part of Python syntax
+
+import keyword
+
+print(keyword.kwlist)
+
+
+keyword.iskeyword("for")     # True
+keyword.iskeyword("while")   # True
+keyword.iskeyword("apple")   # False
+keyword.iskeyword("sum")     # False
