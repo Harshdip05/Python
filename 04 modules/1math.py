7f8323e29de8215286e3afbffc108721e8f3f817
Harshdip Nandre <harshdipnandre05@gmail.com>
2026-01-22 16:01:25 +0000

    Math module

---
+++ 04 modules/1math.py
@@ -0,0 +1,19 @@
+# A module is simply a file that contains Python code.
+        # It can contain:
+        # functions
+        # variables
+
+import math
+# math.
+
+print(math.pi)
+print(math.e)
+print(math.pow(2, 3))  # 2**3
+print(math.log(10))   # base is e
+print(math.log(8, 2)) # base is 2
+print(math.log10(1000))
+print(math.radians(180))
+print(math.degrees(math.pi))
+print(math.factorial(5))
+print(math.exp(1))   # e^1
+print(math.sqrt(16))
