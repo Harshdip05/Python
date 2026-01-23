375c9fead1e4bec703bea2ab171e2b2800ce697e
Harshdip Nandre <harshdipnandre05@gmail.com>
2026-01-23 02:04:26 +0000

    While with else

---
+++ 05 loops/2while_with_else.py
@@ -0,0 +1,13 @@
+# The else block runs ONLY IF:
+        # ✅ the loop finishes normally
+        # ❌ the loop is NOT terminated by break
+
+
+x = 1
+
+while x<3:
+    print(x)
+    x +=1
+
+else :
+    print("limit crossed")    
