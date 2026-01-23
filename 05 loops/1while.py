2cf51c96dc4c639ff02ea5684ef8e5b51fec1566
Harshdip Nandre <harshdipnandre05@gmail.com>
2026-01-23 02:04:10 +0000

    While loop

---
+++ 05 loops/1while.py
@@ -0,0 +1,13 @@
+# A while loop keeps running until a condition becomes False.
+
+
+# print the table
+
+user = int(input("enter number: "))
+print("number is: ",user)
+
+i  = 1
+
+while i<11 :
+    print(user,"*",i,"=",user*i)
+    i +=1
