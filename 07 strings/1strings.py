94deb891d7354eae193cdeef8130c26266d0232e
Harshdip Nandre <harshdipnandre05@gmail.com>
2026-01-24 13:48:59 +0000

    Create and demonstrate Python strings

---
+++ 07 strings/1strings.py
@@ -0,0 +1,41 @@
+# strings
+# unicode
+
+
+# Creating strings
+
+s1 = 'hello'
+s2 = "hello"
+s3 = '''hello'''   # for multi line string
+s4 = """hello"""
+
+print(s1)
+print(s2)
+print(s3)
+print(s4)
+
+text1 = "it's raining"
+text2 = "it\'s raining"  #escape sequence
+text3 = 'it"s raining'
+text4 = '"hello"'
+print(text1)
+print(text2)
+print(text3)
+print(text4)
+
+
+empty = "" # empty string
+print(empty)
+print(type(empty))
+
+num = 100
+print(num,type(num))
+print(str(num),type(str(num)))
+
+
+# python 3 strings are unicode by default
+uni = "🌝 full moon face"
+print(uni)  # no special  syntax needed
+
+# escape characters = \n \t \\ \' \" 
+
