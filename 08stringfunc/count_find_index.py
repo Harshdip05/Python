ac888f7f896f97192e7c799a1d41d74223f87a51
Harshdip Nandre <harshdipnandre05@gmail.com>
2026-01-30 13:21:58 +0000

    Count index and find in strings

---
+++ 08stringfunc/count_find_index.py
@@ -0,0 +1,13 @@
+text = "my name is Harsh"
+
+print(text.count("m"))
+print(text.find("m"))
+print(text.index("m"))
+
+print(text.count("x"))
+print(text.find("x"))
+# print(text.index("x")) 
+
+# Finds the position (index number) of a character or substring
+# Also finds the position, but does NOT give error.
+# Counts how many times a character or substring appears.
